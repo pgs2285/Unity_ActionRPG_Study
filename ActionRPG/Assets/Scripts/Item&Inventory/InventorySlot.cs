5a949f3ddba7adcc476da298eda3ca4adab1c4d7
DESKTOP-NOGAGFD\user <wldnjs2284@naver.com>
2023-10-30 22:49:22 +0900

    no message

--- ActionRPG/Assets/Scripts/Item&Inventory/InventorySlot.cs
+++ ActionRPG/Assets/Scripts/Item&Inventory/InventorySlot.cs
@@ -23,6 +23,7 @@ public class InventorySlot
     {
         get
         {
+            Debug.Log(item.id);
             return item.id >= 0 ? parent.database.ItemObjects[item.id] : null;
         }
         
